126ce9534dea376777dcb25d1cbf207dc2b6e199
nobuhiro-satoh <nobuhiro.satoh@g.pascalia.co.jp>
2021-08-31 13:27:53 +0900

    First run of PHPUnit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,96 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ac0aeb5b074589f6875157e3d9f1620958266516',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ac0aeb5b074589f6875157e3d9f1620958266516',
+    ),
+    'phpunit/php-code-coverage' => 
+    array (
+      'pretty_version' => '1.2.18',
+      'version' => '1.2.18.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe2466802556d3fe4e4d1d58ffd3ccfd0a19be0b',
+    ),
+    'phpunit/php-file-iterator' => 
+    array (
+      'pretty_version' => '3.0.5',
+      'version' => '3.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aa4be8575f26070b100fccb67faabb28f21f66f8',
+    ),
+    'phpunit/php-text-template' => 
+    array (
+      'pretty_version' => '2.0.4',
+      'version' => '2.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5da5f67fc95621df9ff4c4e5a84d6a8a2acf7c28',
+    ),
+    'phpunit/php-timer' => 
+    array (
+      'pretty_version' => '1.0.9',
+      'version' => '1.0.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3dcf38ca72b158baf0bc245e9184d3fdffa9c46f',
+    ),
+    'phpunit/php-token-stream' => 
+    array (
+      'pretty_version' => '1.2.2',
+      'version' => '1.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ad4e1e23ae01b483c16f600ff1bebec184588e32',
+    ),
+    'phpunit/phpunit' => 
+    array (
+      'pretty_version' => '3.7.14',
+      'version' => '3.7.14.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '84750788a409c3d8236153417dc7ca155a4713d2',
+    ),
+    'phpunit/phpunit-mock-objects' => 
+    array (
+      'pretty_version' => '1.2.3',
+      'version' => '1.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5794e3c5c5ba0fb037b11d8151add2a07fa82875',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => 'v2.1.13',
+      'version' => '2.1.13.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '347a7a02204433c6926ecc3f13e805bdc30e8f9f',
+    ),
+  ),
+);
